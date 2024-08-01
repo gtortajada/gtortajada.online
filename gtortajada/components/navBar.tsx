00d13bc536ed923aed0873f33bbf85d5d74532e2
gtortajada <gtortajada1@gmail.com>
2024-08-01 18:31:21 -0300

    Teste de section e criação de navbar

---
+++ gtortajada/components/navBar.tsx
@@ -0,0 +1,10 @@
+// import React from "react" //'Para construir o componente' - não sei a real necessidade por isso vou deixar comentado
+import Link from "next/link" //Para navegação entre páginas sem recarregar
+
+
+
+const navLinks = [
+    {href:"/", text: "Home"},
+    {href:"/servicos", text: "Serviços"},
+    {href:"/contato", text: "Contato"}
+]
--- gtortajada/src/app/page.tsx
+++ gtortajada/src/app/page.tsx
@@ -4,22 +4,22 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-        <section className="text-center" id="homesection">
-          <h1>Section 1</h1>
-          <h2>Home Section</h2>
-        </section>
+      <section className="text-center" id="homesection">
+        <h1>Section 1</h1>
+        <h2>Home Section</h2>
+      </section>
 
-        <section className="text-center" id="abouthsection">
-          <h1>Section 2</h1>
-          <h2>Abouth Pessoal</h2>
-        </section>
+      <section className="text-center" id="abouthsection">
+        <h1>Section 2</h1>
+        <h2>Abouth Pessoal</h2>
+      </section>
 
-        <section className="text-center" id="profsection">
-          <h1>Section 3</h1>
-          <h2>Abouth Profissional</h2>
-        </section>
+      <section className="text-center" id="profsection">
+        <h1>Section 3</h1>
+        <h2>Abouth Profissional</h2>
+      </section>
 
-        
+
     </main>
   );
 }

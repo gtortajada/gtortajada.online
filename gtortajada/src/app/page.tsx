6b7ecbe8ea4b72769bd4963c0d6ee85488493e64
gtortajada <gtortajada1@gmail.com>
2024-08-02 17:04:14 -0300

    Correção e começo de css

--- gtortajada/src/app/page.tsx
+++ gtortajada/src/app/page.tsx
@@ -4,19 +4,19 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <section className="text-center" id="homesection">
+      <section className={styles.heroSection} id="heroSection">
         <h1>Section 1</h1>
         <h2>Home Section</h2>
       </section>
 
       <section className="text-center" id="abouthsection">
         <h1>Section 2</h1>
-        <h2>Abouth Pessoal</h2>
+        <h2>About Pessoal</h2>
       </section>
 
       <section className="text-center" id="profsection">
         <h1>Section 3</h1>
-        <h2>Abouth Profissional</h2>
+        <h2>About Profissional</h2>
       </section>
 
 

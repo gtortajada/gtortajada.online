0e31164054720c963a7e68f429c9b88fde232d98
gtortajada <gtortajada1@gmail.com>
2025-01-22 10:55:17 -0300

    Fix: Correções na Landing Page. -Texto avulso foi comentado para não interferir no código.

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function toggleMode() {
     const html = document.documentElement
     html.classList.toggle("light")
 }
+
+
+// -- Currículo/ LinkedIn --
+
+// Sou um profissional com mais de XX anos de experiência na área YY. Ao longo da minha carreira, tive a oportunidade de participar de grandes projetos (cite alguns projetos e resultados atingidos).
+
+// Possuo sólidos conhecimentos em XX, YY, ZZ (insira palavras-chave e experiência).
+
+// Experiência com sistemas e ferramentas: XX, YY, ZZ (insira os sistemas ERP e ferramentas)
+// Contato: seu e-mail
